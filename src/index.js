c6d681f6501640b3aac755557470e35ef9fb0dad
FabulouS <newbie1690@gmail.com>
2021-07-22 15:15:39 +0400

    refactor: Refactor for optimization

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-const BASE_DICT = {
-    0: "",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-};
+const BASE_NUMBER = ["", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine", "ten", "eleven", "twelve"];
 
-const DECIMAL_DICT = {
+const DECIMAL_NAME = {
     2: "twen",
     3: "thir",
     4: ["four", "for"],
@@ -28,42 +15,37 @@ const DECIMAL_DICT = {
 module.exports = function toReadable(number) {
     // Info from here: https://speakasap.com/en/grammar/chislitelnye/
 
-    let readableNum = "";
-
-    if (number == 0)
-    {
-        readableNum = "zero";
-    }
-    else if (number >= 100)
-    {
-        readableNum = getReadableHundred(number) + getReadableDecimal(number % 100);
-    }
-    else
-    {
-        readableNum = getReadableDecimal(number);
-    }
+    let hundred = Math.floor(number / 100);
+    let readableNum = hundred > 0 ? BASE_NUMBER[hundred] + " hundred " : "";
+    readableNum += getDecimalName(number);
 
     return readableNum.trim();
-
 }
 
-function getReadableHundred(number) {
-    return readableNum = BASE_DICT[Math.floor(number / 100)] + " hundred ";
-}
-
-function getReadableDecimal(number) {
-    if ((number >= 0) && (number < 13))
+function getDecimalName(number) {
+    let remainder = number % 100;
+    if (number == 0)
     {
-        return BASE_DICT[number];
+        return "zero";
     }
-    else if ((number >= 13) && (number <= 19))
+    else if (remainder == 0)
     {
-        let remainder = number % 10;
-        return ((remainder == 4) ? DECIMAL_DICT[remainder][0] : DECIMAL_DICT[remainder]) + "teen";
+        return "";
+    }
+    else if ((remainder > 0) && (remainder < 13))
+    {
+        return BASE_NUMBER[remainder];
+    }
+    else if ((remainder >= 13) && (remainder <= 19))
+    {
+        remainder = number % 10;
+        return ((remainder == 4) ? DECIMAL_NAME[remainder][0] :
+            DECIMAL_NAME[remainder]) + "teen";
     }
     else
     {
-        let decimal = Math.floor(number / 10);
-        return ((decimal == 4) ? DECIMAL_DICT[decimal][1] : DECIMAL_DICT[decimal]) + "ty " + BASE_DICT[number % 10];
+        let decimal = Math.floor(remainder / 10);
+        return ((decimal == 4) ? DECIMAL_NAME[decimal][1] :
+            DECIMAL_NAME[decimal]) + "ty " + BASE_NUMBER[number % 10];
     }
 }
